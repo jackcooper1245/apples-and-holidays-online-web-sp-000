d371a03bc8f1ff2c7b1b465480d58b3c296bd473
Jack Cooper <jackcooper1245@gmail.com>
2020-04-13 10:32:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,6 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 holiday_hash[:winter].each do |parties, supply|
   supply
-  binding.pry
 end
 end
 
