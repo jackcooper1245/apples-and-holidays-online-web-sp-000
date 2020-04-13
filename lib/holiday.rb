a22c22691832123afb9504727782049af2974e48
Jack Cooper <jackcooper1245@gmail.com>
2020-04-13 10:48:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,13 +69,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_holiday = []
 holiday_hash.each do |season, holiday|
   holiday.map do |holiday, supply|
     if supply.include?("BBQ")
-      holiday
+      bbq_holiday << holiday
     end
   end
-end.flatten.compact
+end
+bbq_holiday
 end
 
 
