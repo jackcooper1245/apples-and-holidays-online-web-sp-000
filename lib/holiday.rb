6d8d35b5a85539d3832502f92dfd9ce81d4542f0
Jack Cooper <jackcooper1245@gmail.com>
2020-04-13 10:36:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |season, party, supply|
+holiday_hash.each do |season|
+  puts "#{season.capitalize}"
+  
   
 end
 end
